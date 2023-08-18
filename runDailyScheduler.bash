10f1ff6c667e2d7ec9a0f6d38e2a6adf052ed19c
Sterge <stergios.zissakis@gmail.com>
2023-08-18 06:37:40 +0100

    Bug fixing

--- runDailyScheduler.bash
+++ runDailyScheduler.bash
@@ -2,6 +2,6 @@
 
 source env/bin/activate
 
-#echo "*** Running Daily Scheduler..."
-python -u ./DailyScheduler.py  2>&1 | tee -a logs/daily_scheduler.log 
+echo "*** Running Daily Scheduler..."
+python -u ./DailyScheduler.py $* 2>&1 | tee -a logs/daily_scheduler.log 
 
